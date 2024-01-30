590d37bbdf607b91ee819c79aa5ad00f77d9e781
Marianne Fargnolli <ma.fargnolli@gmail.com>
2024-01-30 19:43:58 -0300

    Exercicio transformacao de graus de fahreinheit para celsius

---
+++ graus.js
@@ -0,0 +1,39 @@
+let F = 73; //fahreinheit
+let C = (F - 32) * 5/9;
+
+Celsius = console.log(C);
+//tentativa 1 de calculo
+
+//apresentou o resultado esperado, porem nao sei o comando para chamar no console o input para o user interagir.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
