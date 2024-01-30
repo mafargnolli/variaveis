0e403dbd8cefaedf215b42e5519385fd72277156
Marianne Fargnolli <ma.fargnolli@gmail.com>
2024-01-30 19:58:27 -0300

    Exercicio desconto ok, porem nao sei usar o input para interacao

---
+++ desconto.js
@@ -0,0 +1,8 @@
+const a = 129.99; //valor do produto
+const b = 100; //representatividade do total do valor do produto
+const c = a - 80.00; //valor do desconto
+const d = c * b / a;
+
+console.log(d);
+
+//apresenta resultado, porem, ainda nao sei usar o input para fazer o user interagir
