b12a0a91e8ec32a8f27c4045bf2e5c5273ac4e36
Marianne Fargnolli <ma.fargnolli@gmail.com>
2024-01-30 20:49:10 -0300

    Exercicio de juros composto incompleto, erro de tipo

---
+++ juroscomposto.js
@@ -0,0 +1,24 @@
+let C = 1.000; //capital de mil reais 
+console.log(C); //imprimindo o capital aqui pra ver se aparece ok
+
+let i = 0.125; //taxa mensal fixa de juros
+console.log(i); //imprimindo a taxa
+
+let t = 5; //periodo do tempo
+console.log(t); //imprimindo o periodo na tela
+
+//let M = 1.000 (1 + i) ** 5; //montante
+//console.log(M);
+//imprime erro de TypeError: 1 is not a function
+
+let cit = (1 + 0.125) ** 5;
+console.log(cit); 
+//o resultado nao entra com o 1.000 a multiplicar, da erro de Tipo informando que 1 nao eh funcao de novo.
+
+
+
+
+
+
+
+
