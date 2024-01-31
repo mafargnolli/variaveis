c04e6c875de487d3ffe722edc8013232caf6b476
Marianne Fargnolli <ma.fargnolli@gmail.com>
2024-01-30 21:01:46 -0300

    Encontrei o resultado da distancia dos dois pontos, porem deve ter uma forma mais facil

---
+++ calculodistancia.js
@@ -0,0 +1,23 @@
+const x2 = 1;
+const x1 = 1;
+
+//console.log(x1, x2); //testando as variaveis
+
+const y2 = 4;
+const y1 = 1;
+
+//console.log(y1, y2);
+
+const x = x2 - x1 ** 2;
+//console.log(x);
+
+const y = (y2 - y1) ** 2;
+//console.log(y);
+
+let d = x + y;
+//console.log(d);
+
+let raiz =  Math.sqrt(d);
+console.log(raiz);
+
+//certamente tem um jeito mais rapido, mas dessa forma cheguei no resultado.
