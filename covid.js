a97ca108312033755a80adaf6fc1c163a8550605
Marianne Fargnolli <ma.fargnolli@gmail.com>
2024-01-30 21:35:21 -0300

    Exercicio pratico descobrindo infectados Covid

---
+++ covid.js
@@ -0,0 +1,12 @@
+//exercicio para simular a quantidade de pessoas infectadas
+let Po = 1.000; //populacao inicial de pessoas infectadas
+let x = 4; //qtd de pessoas para as quais um paciente infectado pode transmitir
+let t = 7; //tempo percorrido
+
+let P = (Po * x) ** 7 / 7; //populaçao infectada apos uma semana eh a populacao inicial multip pela qtd pessoas q podem ser transmitidas por 1 pcte elevado a qt dias
+console.log(P);
+
+
+
+
+
