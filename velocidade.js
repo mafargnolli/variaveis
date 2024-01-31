0122839ec23be1bfefc7d4fe7bce5a43f8b57a3f
Marianne Fargnolli <ma.fargnolli@gmail.com>
2024-01-30 21:07:29 -0300

    Exercicio pratico de conversao de m/s para km/h

---
+++ velocidade.js
@@ -0,0 +1,11 @@
+//velocidade = distPercorrida / tempo
+
+distPercorrida = 500; //metros
+tempo = 10; //segundos
+velocidade = distPercorrida / tempo;
+console.log(velocidade); //imprimindo normal a velocidade em metros por segundo.
+
+//agora preciso converter para km/h.
+conversao = velocidade * 3.6;
+console.log(conversao);
+
