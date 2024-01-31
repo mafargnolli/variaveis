a8770ec994c36141a85603618c54dd03c81055a7
Marianne Fargnolli <ma.fargnolli@gmail.com>
2024-01-30 21:56:48 -0300

    Exercicio Angulos do Poligono

---
+++ angulos.js
@@ -0,0 +1,14 @@
+//soma dos angulos internos de um poligono
+
+//const S = (n-2) * 180; //soma dos angulos internos do poligono
+const N = 6; //numero de lados do poligono
+const A = 720; //valor dos angulos em graus
+
+//calculando a soma dos angulos
+const S = (N - 2) *180;
+console.log(S);
+
+///calculando o valor de cada angulo separadamente
+const a = S / N;
+console.log(a);
+
